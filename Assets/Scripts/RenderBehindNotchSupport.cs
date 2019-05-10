ecac464fdbdd59f5f64da23c5b6c5212ee1028c3
Yury Habets <over17@outlook.com>
2019-05-10 13:12:24 +0200

    Added a public API to change the setting at runtime

--- Assets/Scripts/RenderBehindNotchSupport.cs
+++ Assets/Scripts/RenderBehindNotchSupport.cs
@@ -11,27 +11,33 @@ public class RenderBehindNotchSupport : MonoBehaviour
     private AndroidJavaObject m_Window;
     private AndroidJavaObject m_Windowattributes;
 
-    void Start () {
-	    using (var version = new AndroidJavaClass("android.os.Build$VERSION"))
-	    {
+    public void SetRenderBehindNotch(bool enabled)
+    {
+        using (var version = new AndroidJavaClass("android.os.Build$VERSION"))
+        {
             // Supported on Android 9 Pie (API 28) and later
-	        if (version.GetStatic<int>("SDK_INT") < 28)
-	        {
-	            return;
-	        }
-	    }
-	    using (var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
-	    {
-	        using (var activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
-	        {
-	            m_Window = activity.Call<AndroidJavaObject>("getWindow");
-	            m_Windowattributes = m_Window.Call<AndroidJavaObject>("getAttributes");
-	            m_Windowattributes.Set("layoutInDisplayCutoutMode", RenderBehindNotch ?
+            if (version.GetStatic<int>("SDK_INT") < 28)
+            {
+                return;
+            }
+        }
+        using (var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            using (var activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+            {
+                m_Window = activity.Call<AndroidJavaObject>("getWindow");
+                m_Windowattributes = m_Window.Call<AndroidJavaObject>("getAttributes");
+                m_Windowattributes.Set("layoutInDisplayCutoutMode", enabled ?
                     LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES :
                     LAYOUT_IN_DISPLAY_CUTOUT_MODE_NEVER);
-	            activity.Call("runOnUiThread", new AndroidJavaRunnable(ApplyAttributes));
+                activity.Call("runOnUiThread", new AndroidJavaRunnable(ApplyAttributes));
             }
-	    }
+        }
+    }
+
+    void Start ()
+    {
+        SetRenderBehindNotch(RenderBehindNotch);
     }
 
     void ApplyAttributes()
